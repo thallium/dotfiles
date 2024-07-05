3c64128fd38becee4d44f771e38472cbe281e9b6
Gengchen <tuogengchen@gmail.com>
2024-07-05 10:54:47 -0400

    update wezterm config

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -2,7 +2,7 @@ local wezterm = require 'wezterm'
 local theme = require 'theme'
 local act = wezterm.action
 
-wezterm.on('window-config-reloaded', function(window, pane)
+wezterm.on('window-config-reloaded', function(window, _)
     local overrides = window:get_config_overrides() or {}
     local target = theme.get_color()
     if overrides.colors ~= target.colors then
@@ -12,14 +12,14 @@ wezterm.on('window-config-reloaded', function(window, pane)
     end
 end)
 
-wezterm.on('augment-command-palette', function(window, pane)
+wezterm.on('augment-command-palette', function(_, _)
     return {
         {
             brief = 'Rename tab',
             icon = 'mdi_rename_box',
             action = act.PromptInputLine {
                 description = 'Enter new name for tab',
-                action = wezterm.action_callback(function(window, pane, line)
+                action = wezterm.action_callback(function(window, _, line)
                     if line then
                         window:active_tab():set_title(line)
                     end
@@ -34,35 +34,43 @@ local color = theme.get_color()
 
 return {
     font = wezterm.font_with_fallback {
-        "FiraCode Nerd Font Mono",
-        "Noto Sans CJK SC"
+        -- "Jetbrains Mono",
+        -- "IBM Plex Mono",
+        -- "IntelOne Mono",
+        { family = 'Cascadia Code' },
+        "Symbols Nerd Font",
+        -- "FiraCode Nerd Font",
+        -- "Souce Han Sans SC",
+        { family = "Alibaba PuHuiTi 3.0", weight = "Medium" }
     },
-    font_size = 15,
-    line_height = 1.1,
+    term = 'wezterm',
+    font_size = 17,
+    -- line_height = 0.95,
     colors = color.colors,
     window_frame = color.window_frame,
     tab_bar_at_bottom = true,
     hide_tab_bar_if_only_one_tab = true,
-    default_cursor_style = 'SteadyBar',
-    leader = { key = 'Space', mods = 'CTRL', timeout_milliseconds = 1000 },
+    -- default_cursor_style = 'SteadyBar',
+    default_cursor_style = 'SteadyBlock',
+    -- leader = { key = 'Space', mods = 'CTRL', timeout_milliseconds = 1000 },
     keys = {
         {
             key = 'm',
-            mods = 'LEADER',
+            mods = 'ALT',
             action = act.SplitVertical { domain = 'CurrentPaneDomain' },
         },
         {
             key = ';',
-            mods = 'LEADER',
+            mods = 'ALT',
             action = act.SplitHorizontal { domain = 'CurrentPaneDomain' },
         },
         {
-            key = 'Tab',
-            mods = 'CTRL|ALT|SHIFT',
+            key = ',',
+            mods = 'CTRL|ALT',
             action = act.MoveTabRelative(-1),
         },
         {
-            key = 'Tab',
+            key = '.',
             mods = 'CTRL|ALT',
             action = act.MoveTabRelative(1),
         },
