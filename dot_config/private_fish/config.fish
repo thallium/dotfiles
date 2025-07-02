9d2f15e1375f32df108079536b15025ba54acfc0
Gengchen Tuo <gengchen.tuo@ibm.com>
2025-07-02 14:22:13 -0700

    Fix homebrew init for linux

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -1,5 +1,14 @@
 if status is-interactive
-	eval "$(/opt/homebrew/bin/brew shellenv)"
+	switch (uname)
+		case Darwin
+			if test -x /opt/homebrew/bin/brew
+				eval "$(/opt/homebrew/bin/brew shellenv)"
+			end
+		case Linux
+			if test -x /home/linuxbrew/.linuxbrew/bin/brew
+				eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+			end
+	end
 
 	fish_add_path ~/go/bin
 	fish_add_path '/Applications/Visual Studio Code.app/Contents/Resources/app/bin'
